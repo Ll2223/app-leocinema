bf29c666e09dedb83c27865cc2b1a8a678309d39
Ll2223 <151667423+Ll2223@users.noreply.github.com>
2024-03-11 21:55:02 -0300

    Create cluster-env.sh

---
+++ cluster-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+- kubectl create namespace leocinema
+- kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/cloud/deploy.yaml
