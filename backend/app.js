ac0a0b85f8d506f40e09c305118905c0b1cbf4e2
Ll2223 <151667423+Ll2223@users.noreply.github.com>
2024-03-16 13:04:12 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,11 @@ const Sequelize = require('sequelize');
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
-const PORT = process.env.BACKEND_PORT || 3333;
-const PATH_COMPRAR_INGRESSO = process.env.PATH_COMPRAR_INGRESSO || '/comprar-ingresso';
-const PATH_HEALTH = process.env.PATH_HEALTH || '/health';
+const authMiddleware = require('./auth'); // Importe o middleware de autenticação
 
-// Configuração do Sequelize
+const app = express();
+const PORT = 3333;
+
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql'
@@ -23,22 +22,46 @@ const Ticket = sequelize.define('ticket', {
   quantity: Sequelize.INTEGER
 });
 
-// Sincronizar o modelo com o banco de dados (criar a tabela se não existir)
+// Sincronizar o banco de dados
 sequelize.sync().then(() => {
   console.log('Modelos sincronizados com o banco de dados');
 }).catch(err => {
   console.error('Erro ao sincronizar modelos com o banco de dados:', err);
 });
 
+// Funções para verificar a conexão com o banco de dados e a sincronização do modelo
+const checkDatabaseConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+    return true;
+  } catch (error) {
+    console.error('Não foi possível conectar ao banco de dados:', error);
+    return false;
+  }
+};
+
+const checkModelSync = async () => {
+  try {
+    await Ticket.sync();
+    console.log('Modelo de dados sincronizado com sucesso.');
+    return true;
+  } catch (error) {
+    console.error('Erro ao sincronizar modelo de dados:', error);
+    return false;
+  }
+};
+
 // Middlewares
 app.use(cors({
-  origin: '*' //`${process.env.FRONTEND_URL}:${process.env.FRONTEND_PORT}`// Substitua pela origem do seu frontend
+  origin: '*'
 }));
 app.use(bodyParser.json());
 
 // Rotas
-app.post(PATH_COMPRAR_INGRESSO, async (req, res) => {
+app.post('/comprar-ingresso', authMiddleware, async (req, res) => {
   const { movie, date, time, quantity } = req.body;
+
   try {
     const ticket = await Ticket.create({
       movie,
@@ -53,8 +76,15 @@ app.post(PATH_COMPRAR_INGRESSO, async (req, res) => {
   }
 });
 
-app.get(PATH_HEALTH, (req, res) => {
-  res.send('OK');
+app.get('/health', async (req, res) => {
+  const databaseConnection = await checkDatabaseConnection();
+  const modelSyncStatus = await checkModelSync();
+
+  if (databaseConnection && modelSyncStatus) {
+    res.send('OK');
+  } else {
+    res.status(500).json({ error: 'A aplicação não está funcionando corretamente.' });
+  }
 });
 
 // Iniciar o servidor
