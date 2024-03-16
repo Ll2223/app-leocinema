cecb38a48e96a0e914483c7749057e4e0a66b365
Ll2223 <151667423+Ll2223@users.noreply.github.com>
2024-03-16 13:06:17 -0300

    Update build.js

--- frontend/build.js
+++ frontend/build.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
 
-// Carrega as variáveis de ambiente do arquivo .env
 dotenv.config();
 
-// Substitui as variáveis de ambiente no arquivo script.js
 const scriptJsPath = './script.js';
 let scriptJsContent = fs.readFileSync(scriptJsPath, 'utf8');
 scriptJsContent = scriptJsContent.replace(/process\.env\.BACKEND_URL/g, `'${process.env.BACKEND_URL}'`);
