561f71a075a8e9283f149804b3e138686af70a2c
Ll2223 <151667423+Ll2223@users.noreply.github.com>
2024-03-16 13:07:44 -0300

    Update cluster-env.sh

--- cluster-env.sh
+++ cluster-env.sh
@@ -2,3 +2,7 @@
 
 - kubectl create namespace leocinema
 - kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/cloud/deploy.yaml
+- docker build -t leobgs/leocinema-backend-test:beta-final2 .
+- docker push leobgs/leocinema-backend-test:beta-final2
+- docker build -t leobgs/leocinema-frontend-test:beta-final .
+- docker push leobgs/leocinema-frontend-test:beta-final
